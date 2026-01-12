330af452b51abdc4d04c8777c8c0217dc5288b6f
Duartch-pt <vascoduartemultimedia@gmail.com>
2026-01-12 17:06:51 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,18 +3,21 @@ const WORKER_URL = "https://tribalwars.vascoduartemultimedia.workers.dev/";
 async function loadPlayers() {
     try {
         const response = await fetch(WORKER_URL, { mode: 'cors' });
-        const data = await response.json(); // agora JSON
+        const data = await response.json(); // JSON do Worker
+
+        // Ordenar por pontos descrescente (ranking)
+        data.sort((a, b) => parseInt(b.points) - parseInt(a.points));
 
         const tbody = document.querySelector("#playersTable tbody");
         tbody.innerHTML = "";
 
-        data.forEach(player => {
+        data.forEach((player, index) => {
             const tr = document.createElement("tr");
             tr.innerHTML = `
+                <td>${index + 1}</td> <!-- Rank -->
                 <td>${player.name}</td>
-                <td>-</td>
-                <td>-</td>
-                <td>-</td>
+                <td>${player.points}</td>
+                <td>${player.villages}</td>
             `;
             tbody.appendChild(tr);
         });
